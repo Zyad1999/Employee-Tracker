057d4f00d816e5bd1565585e4bd0026b1da8714c
mahmoud reda <55753782+MAHMOUDRR707@users.noreply.github.com>
2022-05-22 19:28:57 +0200

    dependent  tabel keys

--- employeeTracker/entities/models/dependent.py
+++ employeeTracker/entities/models/dependent.py
@@ -4,13 +4,17 @@ from typing import List
 from sqlalchemy.orm import relationship
 from config.database import Base
 from .employee import Employee
+from sqlalchemy.schema import PrimaryKeyConstraint
 
 class Dependent(Base):
 
     __tablename__ = 'Dependent'
 
-    name = Column(String)
+    name = Column(String,primary_key=True)
     gender = Column(String)
     relation = Column(String)
     employee_ssn = Column(Integer, ForeignKey(Employee.ssn, ondelete="CASCADE"),primary_key=True )
     employee = relationship(Employee, back_populates="dependents")
+    __table_args__ = (
+        PrimaryKeyConstraint(name, employee_ssn),
+    )
